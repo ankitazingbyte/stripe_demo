dbfcc3c38cd782ac8bf55ceb00aba3e1d244f500
ankita dangi <ankita.dangi@zingbytes.com>
2017-12-14 11:20:40 +0530

    update key

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ENV['PUBLISHABLE_KEY'],
-  :secret_key      => ENV['SECRET_KEY']
+  :publishable_key => 'pk_test_RE7y1XcDVS3iDCt4BLIMM6x4',
+  :secret_key      => 'sk_test_f0kppmcboNfj77D8s0vYTYEO'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
